4fafef0dfe86999dff7080de2bffcb6079c6bcfb
Ricardo Lopes <ricardo.lopes@locaweb.com.br>
2015-09-25 20:33:19 -0300

    adding routes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
-#  add_foreign_key :comments, :summary
-  belongs_to :summary
+    #  add_foreign_key :comment, :summary
+    belongs_to :user
+    belongs_to :summary
 end
