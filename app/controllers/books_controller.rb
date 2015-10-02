4130d6d17360dfe1037a4685eb24d7b27678788f
Ricardo Lopes <ricardo.lopes@locaweb.com.br>
2015-10-02 19:26:22 -0300

    adding files books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,46 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+def new
+    @book = Book.new
+      @books = Book.all
+end
+
+def edit
+  @book = Book.find(params[:id])
+  @books = Book.all
+  render :action => 'index'
+end
+
+def create
+    @book = Book.new(params[:book])
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Livro criado com sucesso.' }
+      else
+        format.html { render action: "new" }
+      end
+    end
+end
+
+  def update
+    @book = Book.find(params[:id])
+    respond_to do |format|
+      if @book.update_attributes(params[:book])
+        format.html { redirect_to @book, notice: 'Livro atualizado com sucesso' }
+      else
+        format.html { render action: "edit" }
+      end
+    end
+  end
+
+def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_path }
+end
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'books#index'
-  resources :books, only: [:index, :show]
+  resources :books, only: [:index, :show, :new, :edit]
   resources :comments
   resources :summaries
 
