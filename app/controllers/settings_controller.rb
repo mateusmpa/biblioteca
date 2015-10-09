7dbf4a830e122ec2a0e4e2c14d9465d27fec7287
Ricardo Lopes <ricardo.lopes@locaweb.com.br>
2015-10-09 20:22:34 -0300

    changing files settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,39 +1,65 @@
 class SettingsController < ApplicationController
-  before_action :set_setting, only: [:show, :edit, :update]
+  before_action :set_setting, only: [:show, :edit, :update, :destroy]
+
+  # GET /settinges
+  # yersh GET /settinges.json
   def index
     @settings = Setting.all
   end
 
+  # GET /settinges/1
+  # GET /settinges/1.json
   def show
-   @setting =  Setting.find(params[:id])
+    # GET thingy
   end
 
-  # GET /settings/1/edit
+
+  # GET /settinges/sdfnew
+  def new
+    @setting = Setting.new
+  end
+
+  # GET /settinges/1/edit
   def edit
-    @setting = Setting.find(params[:id])
   end
 
-  # POST /settings
+  # POST /settinges
+  # POST /settinges.json
   def create
     @setting = Setting.new(setting_params)
 
     respond_to do |format|
       if @setting.save
-        format.html { redirect_to @setting, notice: 'Configuração criada com sucesso.' }
+        format.html { redirect_to @setting, notice: 'Setting was successfully created.' }
+        format.json { render action: 'show', setting: :created, location: @setting }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
+        format.json { render json: @setting.errors, setting: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /settings/1
+  # PATCH/PUT /settinges/1
+  # PATCH/PUT /settinges/1.json
   def update
     respond_to do |format|
       if @setting.update(setting_params)
-        format.html { redirect_to @setting, notice: 'Configuração atualizada com sucesso.' }
+        format.html { redirect_to @setting, notice: 'Setting was successfully updated.' }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
-       end
+        format.html { render action: 'edit' }
+        format.json { render json: @setting.errors, setting: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /settinges/1
+  # DELETE /settinges/1.json
+  def destroy
+    @setting.destroy
+    respond_to do |format|
+      format.html { redirect_to settinges_url }
+      format.json { head :no_content }
     end
   end
 
@@ -45,6 +71,6 @@ class SettingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def setting_params
-      params[:setting].permit(:name, :title, :domain, :url, :email )
+      params.require(:setting).permit(:nothing)
     end
 end
