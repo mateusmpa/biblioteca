13c18e47bd4baaec89ea17fdf0eedb737dc797c4
Ricardo Lopes <ricardo.lopes@locaweb.com.br>
2015-10-13 19:53:48 -0300

    add new files for setting - changing edit

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,76 +1,34 @@
 class SettingsController < ApplicationController
-  before_action :set_setting, only: [:show, :edit, :update, :destroy]
-
-  # GET /settinges
-  # yersh GET /settinges.json
+  before_action :set_setting, only: [:show, :edit, :update]
   def index
     @settings = Setting.all
   end
 
-  # GET /settinges/1
-  # GET /settinges/1.json
   def show
-    # GET thingy
+   @setting =  Setting.find(params[:id])
   end
 
 
-  # GET /settinges/sdfnew
-  def new
-    @setting = Setting.new
-  end
-
-  # GET /settinges/1/edit
   def edit
+    @setting = Setting.find(params[:id])
   end
 
-  # POST /settinges
-  # POST /settinges.json
-  def create
-    @setting = Setting.new(setting_params)
-
-    respond_to do |format|
-      if @setting.save
-        format.html { redirect_to @setting, notice: 'Setting was successfully created.' }
-        format.json { render action: 'show', setting: :created, location: @setting }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @setting.errors, setting: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /settinges/1
-  # PATCH/PUT /settinges/1.json
   def update
     respond_to do |format|
       if @setting.update(setting_params)
-        format.html { redirect_to @setting, notice: 'Setting was successfully updated.' }
-        format.json { head :no_content }
+        format.html { redirect_to @setting, notice: 'Livro atualizado com sucesso.' }
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @setting.errors, setting: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /settinges/1
-  # DELETE /settinges/1.json
-  def destroy
-    @setting.destroy
-    respond_to do |format|
-      format.html { redirect_to settinges_url }
-      format.json { head :no_content }
+        format.html { render :edit }
+       end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_setting
       @setting = Setting.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def setting_params
-      params.require(:setting).permit(:nothing)
+      params[:setting].permit(:name, :title, :domain, :url, :email )
     end
 end
