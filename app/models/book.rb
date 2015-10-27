cea4e7ba6a7c36b166d568fec31c0f88d57e29ea
Ricardo Lopes <ricardo.lopes@locaweb.com.br>
2015-10-27 14:08:27 -0200

    adding model author

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ActiveRecord::Base
   has_many :authors
   validates :author, presence: true
   validates :title, presence: true,
-             length: { :minimum=> 5, :maximum => 140, :message => "O título deve ter entre 5 e 140 caracteres."}
+             length: { :maximum => 140, :message => "O título deve ter no máximo 140 caracteres."}
 
   def self.search(search)
      where("title LIKE ?", "%#{search.to_s.downcase}%")
